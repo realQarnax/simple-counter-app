924704ec787179d4e8db249297f6d619ed4a34b8
Akram <47157066+akramsoltani@users.noreply.github.com>
2021-09-01 13:40:21 +0100

    Code update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import  {useState, useEffect} from 'react';
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${counter} times`;
+  }, [counter]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center'}}>
+      <h1>Counter App</h1>
+      <h3>Click the button to increment:</h3>
+      <h4>{counter}</h4>
+      <button onClick = {() => setCounter(counter + 1)}>Click Me!</button>
     </div>
   );
 }
-
-export default App;
